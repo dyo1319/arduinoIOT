1312f625cf3e8cd86f8d3dc340c3d81846e906e0
TerminatorGodx <110170433+dyo1319@users.noreply.github.com>
2024-08-04 21:20:27 +0300

    קוד שמזהה לחיצת כפתור אם נלחץ או לא
    קוד שעושה חישוב זמן לחיצת כפתור ו מזהה לחיצה

---
+++ ardsof/ardsof.ino
@@ -0,0 +1,43 @@
+#define btnPin D5
+
+int lastVal;
+unsigned long lastPressTime;
+unsigned long pressStartTime = 0;
+unsigned long pressDuration = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(btnPin, INPUT_PULLUP);
+
+  lastPressTime = millis();
+  lastVal = digitalRead(btnPin);
+}
+
+void loop() {
+  int val = digitalRead(btnPin);
+
+  if((val == LOW) && (lastVal == HIGH) && (millis() - lastPressTime > 50)){
+    lastPressTime = millis();
+    Serial.println(lastPressTime);
+  }
+
+  measurePressDuration(val);
+
+  lastVal = val;
+}
+
+void measurePressDuration(int btnCurrVal) {
+  if (btnCurrVal == LOW) {
+    if (pressStartTime == 0) {
+      pressStartTime = millis();
+    }
+  } else {
+    if (pressStartTime != 0) {
+      pressDuration = millis() - pressStartTime;
+      Serial.print("Press duration: ");
+      Serial.print(pressDuration);
+      Serial.println(" milliseconds");
+      pressStartTime = 0;
+    }
+  }
+}
