73bc4edc6d5c29a442e0ad8c10c21567ab2423ff
TerminatorGodx <110170433+dyo1319@users.noreply.github.com>
2024-08-05 19:28:10 +0300

    הוספת מוד
    מחובר לאדמה נכנסים ל  תוך הטבלה של 10 לחיצות אחרנות אחרת נכנסים לקוד של לחיצה כדי לעדכן בשרת  אבל כרגע חסר תצוגה של הטבלה ו הפעלת שרת פנימי חוץ מזה הקוד תקין הוספת מוד אם D6 מחובר לאדמה נכנסים

--- ardsof/ardsof.ino
+++ ardsof/ardsof.ino
@@ -1,42 +1,21 @@
-#define btnPin D5
-
-int lastVal;
-unsigned long lastPressTime;
-unsigned long pressStartTime = 0;
-unsigned long pressDuration = 0;
-
+#define SETTING_PIN D6
+//Connect pin D6 to ground to Led Work  Leave pin D6 floating or connect it to 5V to Btn work;
+bool IhaveLed;
 void setup() {
   Serial.begin(9600);
-  pinMode(btnPin, INPUT_PULLUP);
+  pinMode(SETTING_PIN,INPUT_PULLUP);
+  IhaveLed = (digitalRead(SETTING_PIN) == LOW) ? true:false;
 
-  lastPressTime = millis();
-  lastVal = digitalRead(btnPin);
-  wifiClient_Setup();
-}
-
-void loop() {
-  int val = digitalRead(btnPin);
-
-  if((val == LOW) && (lastVal == HIGH) && (millis() - lastPressTime > 50)){
-    lastPressTime = millis();
-    Serial.println(lastPressTime);
-  }
-  measurePressDuration(val);
-  lastVal = val;
-}
-
-void measurePressDuration(int btnCurrVal) {
-  if (btnCurrVal == LOW) {
-    if (pressStartTime == 0) {
-      pressStartTime = millis();
-    }
+  if(IhaveLed){
+   // LED_setup();
   } else {
-    if (pressStartTime != 0) {
-      pressDuration = millis() - pressStartTime;
-      Serial.print("Press duration: ");
-      Serial.print(pressDuration);
-      Serial.println(" milliseconds");
-      pressStartTime = 0;
-    }
+    btn_setup();
+  }
+}
+void loop() {
+  if(IhaveLed){
+    //LED_loop();
+  } else {
+    btn_loop();
   }
 }
--- ardsof/k20_RemoteWiFi.ino
+++ ardsof/k20_RemoteWiFi.ino
@@ -3,29 +3,62 @@
 #include <WiFiUdp.h>
 #include <ESP8266HTTPClient.h>
 
-const char* ssid = "raad";
-const char* pswd = "";
-
+const char* ssid = "RaadHeno";
+const char* pswd = "Raad12340";
 
 WiFiClient client;
-int server_port = 80;//http
+int server_port = 80; //http
 
 void wifiClient_Setup() {
   Serial.println("wifiSetup");
-  WiFi.begin(ssid);
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.println("trying ...");
-        delay(100);
-    }
-    Serial.println("Connected to network");
+  WiFi.begin(ssid, pswd);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("trying ...");
+    delay(100);
+  }
+  Serial.println("Connected to network");
 }
 
-  void SendBtnPressed(){
-      HTTPClient http;
-      String dataURL = "http://55.55.55.55/Toggle";
-      http.begin(client, dataURL);
-      int httpCode = http.GET();
-      http.end();
-  }
-  
+void SendBtnPressed() {
+  HTTPClient http;
+  const int deviceID = 1121; // מספר רכיב שלך
+  const int channel = 1;   // מספר ערוץ שלך
 
+  String dataURL = "http://api.kits4.me/GEN/api.php?ACT=SET&DEV=" + String(deviceID) + "&CH=" + String(channel) + "&VAL=" + String(pressDuration);
+
+  http.begin(client, dataURL);
+
+  int httpCode = http.POST("");
+
+  if (httpCode > 0) {
+    String payload = http.getString();
+    Serial.println("Response: " + payload);
+  } else {
+    Serial.println("Error in HTTP request");
+  }
+  http.end();
+}
+
+long readCurrentDurationFromServer() {
+  HTTPClient http;
+  const int deviceID = 1121; // מספר רכיב שלך
+  const int channel = 1;   // מספר ערוץ שלך
+
+  String dataURL = "http://api.kits4.me/GEN/api.php?ACT=GET&DEV=" + String(deviceID) + "&CH=" + String(channel);
+
+  http.begin(client, dataURL);
+
+  int httpCode = http.GET();
+  long currentDuration = -1;
+
+  if (httpCode > 0) {
+    String payload = http.getString();
+    Serial.println("Response: " + payload);
+    currentDuration = payload.toInt();
+  } else {
+    Serial.println("Error in HTTP request");
+  }
+  http.end();
+
+  return currentDuration;
+}
